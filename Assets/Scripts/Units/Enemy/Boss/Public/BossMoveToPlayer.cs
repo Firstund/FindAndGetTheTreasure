dea11faa68e0ed6a7d7192f6e8eea82009ce35cf
Firstund <yjw040608@gmail.com>
2021-11-30 01:31:55 +0900

    간단하게 코드 정리

--- Assets/Scripts/Units/Enemy/Boss/Public/BossMoveToPlayer.cs
+++ Assets/Scripts/Units/Enemy/Boss/Public/BossMoveToPlayer.cs
@@ -16,9 +16,6 @@ public class BossMoveToPlayer : MonoBehaviour, IBossSkill
     [SerializeField]
     private string doAnimationTriggerName = "";
 
-    [SerializeField]
-    private LayerMask whatIsGround;
-
     private Vector2 targetPos = Vector2.zero;
     private Vector2 moveDir
     {
